e1b5f6c82c72b614004d65f391d6be4cbf35d49f
sejineer <jinpark99@naver.com>
2024-06-21 01:19:08 +0900

    [FEAT]: AUTH Package 구성 및 SecurityConfig 설정과 JWT Dependency 추가

--- core/core-api/build.gradle.kts
+++ core/core-api/build.gradle.kts
@@ -16,4 +16,11 @@ dependencies {
     testImplementation(project(":tests:api-docs"))
 
     implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.security:spring-security-oauth2-client")
+
+    // JWT
+    implementation("io.jsonwebtoken:jjwt-api:0.12.5")
+    implementation("io.jsonwebtoken:jjwt-impl:0.12.5")
+    implementation("io.jsonwebtoken:jjwt-jackson:0.12.5")
 }

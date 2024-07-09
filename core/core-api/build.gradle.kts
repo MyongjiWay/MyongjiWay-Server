37faaa2baf40d9495560adc0c3fcc08dd2d7ea63
sejineer <jinpark99@naver.com>
2024-07-10 04:34:45 +0900

    [DEPS]: validation, context dependency 추가

--- core/core-api/build.gradle.kts
+++ core/core-api/build.gradle.kts
@@ -11,6 +11,7 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.security:spring-security-oauth2-client")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
 
     // JWT
     implementation("io.jsonwebtoken:jjwt-api:0.12.6")

a292ea1787832def264dbe1f652147200e83b9f0
hojinida <opqrs1103@gmail.com>
2024-07-16 07:58:53 +0900

    [DOCS]: Docs 버전 추가(#15)

--- core/core-api/build.gradle.kts
+++ core/core-api/build.gradle.kts
@@ -7,16 +7,15 @@ dependencies {
 
     testImplementation(project(":tests:api-docs"))
 
-    // 기본
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springframework.security:spring-security-oauth2-client")
     implementation("org.springframework.boot:spring-boot-starter-validation")
 
     // JWT
-    implementation("io.jsonwebtoken:jjwt-api")
-    implementation("io.jsonwebtoken:jjwt-impl")
-    implementation("io.jsonwebtoken:jjwt-jackson")
+    implementation("io.jsonwebtoken:jjwt-api:0.12.6")
+    implementation("io.jsonwebtoken:jjwt-impl:0.12.6")
+    implementation("io.jsonwebtoken:jjwt-jackson:0.12.6")
 }
 
 tasks.getByName("bootJar") {

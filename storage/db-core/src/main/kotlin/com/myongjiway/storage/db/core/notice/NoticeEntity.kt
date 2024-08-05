d9e16ba5be48bc63e451b321c9f726774d38318a
hojinida <opqrs1103@gmail.com>
2024-08-05 20:28:45 +0900

    [FEAT] ERROR 코드 추가 (#22)

--- storage/db-core/src/main/kotlin/com/myongjiway/storage/db/core/notice/NoticeEntity.kt
+++ storage/db-core/src/main/kotlin/com/myongjiway/storage/db/core/notice/NoticeEntity.kt
@@ -1,6 +1,6 @@
 package com.myongjiway.storage.db.core.notice
 
-import com.myongjiway.domain.notice.Notice
+import com.myongjiway.notice.Notice
 import com.myongjiway.storage.db.core.common.BaseEntity
 import jakarta.persistence.Entity
 import jakarta.persistence.Table
@@ -10,20 +10,17 @@ import jakarta.persistence.Table
 class NoticeEntity(
     private var title: String,
     private var content: String,
-    private var author: String,
 ) : BaseEntity() {
     fun toNotice() = Notice(
         id = id!!,
         title = title,
         content = content,
-        author = author,
         createdAt = createdAt,
         updatedAt = updatedAt,
     )
 
-    fun update(title: String, content: String, author: String) {
+    fun update(title: String, content: String) {
         this.title = title
         this.content = content
-        this.author = author
     }
 }

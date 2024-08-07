761120b87bc49202e545b3edeb60102f0a4e140f
Jang Hojin <96263955+hojinida@users.noreply.github.com>
2024-08-08 03:20:04 +0900

    [Hotfix] build gradle fix (#53)
    * [HOTFIX] build.gradle.kts 의존성 수정 (#47)
    
    * [HOTFIX] build.gradle.kts 의존성 수정 (#47)
    
    * [HOTFIX] build.gradle.kts 의존성 수정 (#47)
    
    * [FEAT] favicon Controller 추가 (#47)
    
    * [FEAT] .gitignore 추가 (#47)

---
+++ core/core-api/src/main/kotlin/com/myongjiway/core/auth/controller/FaviconController.kt
@@ -0,0 +1,11 @@
+package com.myongjiway.core.auth.controller
+
+import com.myongjiway.core.api.support.response.ApiResponse
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class FaviconController {
+    @GetMapping("/favicon.ico")
+    fun favicon(): ApiResponse<Any> = ApiResponse.success()
+}

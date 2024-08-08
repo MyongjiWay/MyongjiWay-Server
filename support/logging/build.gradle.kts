94e439784f8a166b446bf1cf9dd0e94c2ea32b95
hojinida <opqrs1103@gmail.com>
2024-08-09 00:29:12 +0900

    [Docs] logback 의존성 추가 (#55)

--- support/logging/build.gradle.kts
+++ support/logging/build.gradle.kts
@@ -1,4 +1,5 @@
 dependencies {
     implementation("io.micrometer:micrometer-tracing-bridge-brave")
+    implementation("net.logstash.logback:logstash-logback-encoder:8.0")
     implementation("io.sentry:sentry-logback:${property("sentryVersion")}")
 }

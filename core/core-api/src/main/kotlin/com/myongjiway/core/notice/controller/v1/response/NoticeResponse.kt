80f7e5f0e5e43c79bff3b23e73cd192b2a1355b7
hojinida <opqrs1103@gmail.com>
2024-08-09 22:32:10 +0900

    [FEAT] Author, CreateAt 추가 (#60)

--- core/core-api/src/main/kotlin/com/myongjiway/core/notice/controller/v1/response/NoticeResponse.kt
+++ core/core-api/src/main/kotlin/com/myongjiway/core/notice/controller/v1/response/NoticeResponse.kt
@@ -1,12 +1,15 @@
 package com.myongjiway.core.notice.controller.v1.response
 
 import com.myongjiway.notice.Notice
+import java.time.LocalDateTime
 
 data class NoticeResponse(
     val id: Long,
     val title: String,
+    val author: String,
     val content: String,
     val read: Boolean = false,
+    val createdAt: LocalDateTime?,
 ) {
     companion object {
         fun of(
@@ -14,8 +17,10 @@ data class NoticeResponse(
         ): NoticeResponse = NoticeResponse(
             id = notice.id!!,
             title = notice.title,
+            author = notice.author,
             content = notice.content,
             read = notice.read,
+            createdAt = notice.createdAt,
         )
     }
 }

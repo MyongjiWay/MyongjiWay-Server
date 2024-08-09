3c978ffcc667f584a9f2f350f89b1eea994dd97b
hojinida <opqrs1103@gmail.com>
2024-08-09 19:08:15 +0900

    [FEAT] @AuthenticationPrincipal 유저 주입 (#60)

--- core/core-api/src/main/kotlin/com/myongjiway/core/notice/controller/NoticeController.kt
+++ core/core-api/src/main/kotlin/com/myongjiway/core/notice/controller/NoticeController.kt
@@ -4,6 +4,8 @@ import com.myongjiway.core.api.support.response.ApiResponse
 import com.myongjiway.core.notice.controller.v1.request.NoticeRequest
 import com.myongjiway.core.notice.controller.v1.response.NoticeResponse
 import com.myongjiway.notice.NoticeService
+import com.myongjiway.user.User
+import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
@@ -19,26 +21,26 @@ class NoticeController(
     private val noticeService: NoticeService,
 ) {
     @PostMapping("/domain/notices")
-    fun createNotice(@RequestBody noticeRequest: NoticeRequest): ApiResponse<Any> {
-        noticeService.createNotice(noticeRequest.toNotice())
+    fun createNotice(@RequestBody noticeRequest: NoticeRequest, @AuthenticationPrincipal user: User): ApiResponse<Any> {
+        noticeService.createNotice(noticeRequest.toNotice(), user)
         return ApiResponse.success()
     }
 
     @PutMapping("/domain/notices/{noticeId}")
-    fun updateNotice(@PathVariable noticeId: Long, @RequestBody noticeRequest: NoticeRequest): ApiResponse<Any> {
-        noticeService.updateNotice(noticeId, noticeRequest.toNotice())
+    fun updateNotice(@PathVariable noticeId: Long, @RequestBody noticeRequest: NoticeRequest, @AuthenticationPrincipal user: User): ApiResponse<Any> {
+        noticeService.updateNotice(noticeId, noticeRequest.toNotice(), user)
         return ApiResponse.success()
     }
 
     @DeleteMapping("/domain/notices/{noticeId}")
-    fun deleteNotice(@PathVariable noticeId: Long): ApiResponse<Any> {
-        noticeService.deleteNotice(noticeId)
+    fun deleteNotice(@PathVariable noticeId: Long, @AuthenticationPrincipal user: User): ApiResponse<Any> {
+        noticeService.deleteNotice(noticeId, user)
         return ApiResponse.success()
     }
 
     @GetMapping("/notices/{noticeId}")
-    fun getNotice(@PathVariable noticeId: Long): ApiResponse<NoticeResponse> = ApiResponse.success(NoticeResponse.of(noticeService.getNotice(noticeId)))
+    fun getNotice(@PathVariable noticeId: Long, @AuthenticationPrincipal user: User): ApiResponse<NoticeResponse> = ApiResponse.success(NoticeResponse.of(noticeService.getNotice(noticeId, user)))
 
     @GetMapping("/notices/all")
-    fun getAllNotices(): ApiResponse<List<NoticeResponse>> = ApiResponse.success(noticeService.getNotices().map { NoticeResponse.of(it) })
+    fun getAllNotices(@AuthenticationPrincipal user: User): ApiResponse<List<NoticeResponse>> = ApiResponse.success(noticeService.getNotices(user).map { NoticeResponse.of(it) })
 }

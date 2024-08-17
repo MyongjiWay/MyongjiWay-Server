a0c528cb96841eb5b2cf3f46a7bbf3c8f49893e2
sejineer <jinpark99@naver.com>
2024-08-17 15:26:06 +0900

    [CHORE]: KLint 오류 수정

--- clients/client-apple/src/main/kotlin/com/myongjiway/client/apple/AppleRestClientConfig.kt
+++ clients/client-apple/src/main/kotlin/com/myongjiway/client/apple/AppleRestClientConfig.kt
@@ -5,5 +5,4 @@ import org.springframework.context.annotation.Configuration
 
 @Configuration
 @ConfigurationProperties(prefix = "apple")
-internal class AppleRestClientConfig {
-}
+internal class AppleRestClientConfig

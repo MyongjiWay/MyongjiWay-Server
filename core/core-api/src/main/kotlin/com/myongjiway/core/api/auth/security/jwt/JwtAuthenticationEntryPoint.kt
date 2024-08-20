aff6af229c0691fdbf221464620c3cc08c80bb1e
Park Sejin <jinpark99@naver.com>
2024-08-21 03:29:32 +0900

    [FIX]: 인증/인가 응답 방식 수정

--- core/core-api/src/main/kotlin/com/myongjiway/core/api/auth/security/jwt/JwtAuthenticationEntryPoint.kt
+++ core/core-api/src/main/kotlin/com/myongjiway/core/api/auth/security/jwt/JwtAuthenticationEntryPoint.kt
@@ -1,5 +1,6 @@
 package com.myongjiway.core.api.auth.security.jwt
 
+import com.myongjiway.core.api.support.error.CoreApiException
 import com.myongjiway.core.api.support.error.ErrorType
 import jakarta.servlet.http.HttpServletRequest
 import jakarta.servlet.http.HttpServletResponse
@@ -16,41 +17,34 @@ class JwtAuthenticationEntryPoint : AuthenticationEntryPoint {
         authException: AuthenticationException,
     ) {
         val exception = request.getAttribute("exception") as? String
-        when (exception) {
-            "NotExistUser" -> {
-                setResponse(response, ErrorType.NOT_EXIST_USER_ERROR)
-            }
-
-            "SignatureException" -> {
-                setResponse(response, ErrorType.INVALID_TOKEN_ERROR)
-            }
-
-            "ExpiredJwtException" -> {
-                setResponse(response, ErrorType.EXPIRED_JWT_ERROR)
-            }
-
-            "MalformedJwtException" -> {
-                setResponse(response, ErrorType.INVALID_TOKEN_ERROR)
-            }
-
-            "HiJackException" -> {
-                setResponse(response, ErrorType.HIJACK_JWT_TOKEN_ERROR)
-            }
-
-            null -> {
-                setResponse(response, ErrorType.UNAUTHORIZED_ERROR)
-            }
+        val errorType = when (exception) {
+            "NotExistUser" -> ErrorType.NOT_EXIST_USER_ERROR
+            "SignatureException" -> ErrorType.INVALID_TOKEN_ERROR
+            "ExpiredJwtException" -> ErrorType.EXPIRED_JWT_ERROR
+            "MalformedJwtException" -> ErrorType.INVALID_TOKEN_ERROR
+            "HiJackException" -> ErrorType.HIJACK_JWT_TOKEN_ERROR
+            else -> ErrorType.UNAUTHORIZED_ERROR
         }
+        val apiException = CoreApiException(errorType)
+
+        setResponse(response, apiException)
     }
 
-    private fun setResponse(response: HttpServletResponse, errorType: ErrorType) {
+    private fun setResponse(response: HttpServletResponse, exception: CoreApiException) {
         response.contentType = "application/json;charset=UTF-8"
         response.characterEncoding = "UTF-8"
-        response.status = HttpServletResponse.SC_UNAUTHORIZED
+        response.status = exception.errorType.status.value()
         val json = JSONObject().apply {
-            put("code", errorType.code)
-            put("message", errorType.message)
-            put("isSuccess", false)
+            put("result", "ERROR")
+            put("data", null)
+            put(
+                "error",
+                JSONObject().apply {
+                    put("code", exception.errorType.code)
+                    put("message", exception.errorType.message)
+                    put("data", exception.data)
+                },
+            )
         }
         response.writer.print(json)
     }

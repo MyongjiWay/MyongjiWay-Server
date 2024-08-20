87fec41648b4dc1a512dde62f1fc07e30d820381
hojinida <opqrs1103@gmail.com>
2024-08-21 01:01:07 +0900

    [FEAT] Advice 에서 전역에러 로깅하는 것으로 변경 (#99)

--- core/core-api/src/main/kotlin/com/myongjiway/core/api/controller/ApiControllerAdvice.kt
+++ core/core-api/src/main/kotlin/com/myongjiway/core/api/controller/ApiControllerAdvice.kt
@@ -4,6 +4,8 @@ import com.myongjiway.core.api.support.error.CoreApiException
 import com.myongjiway.core.api.support.error.ErrorType
 import com.myongjiway.core.api.support.response.ApiResponse
 import com.myongjiway.core.domain.error.CoreException
+import org.slf4j.LoggerFactory
+import org.slf4j.MDC
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
@@ -11,11 +13,35 @@ import org.springframework.web.bind.annotation.RestControllerAdvice
 @RestControllerAdvice
 class ApiControllerAdvice {
     @ExceptionHandler(CoreApiException::class)
-    fun handleCoreApiException(e: CoreApiException): ResponseEntity<ApiResponse<Any>> = ResponseEntity(ApiResponse.error(e.errorType, e.data), e.errorType.status)
+    fun handleCoreApiException(e: CoreApiException): ResponseEntity<ApiResponse<Any>> {
+        MDC.put("status", e.errorType.status.toString())
+        logging(e)
+        return ResponseEntity(ApiResponse.error(e.errorType, e.data), e.errorType.status)
+    }
 
     @ExceptionHandler(Exception::class)
-    fun handleException(e: Exception): ResponseEntity<ApiResponse<Any>> = ResponseEntity(ApiResponse.error(ErrorType.DEFAULT_ERROR, e.message), ErrorType.DEFAULT_ERROR.status)
+    fun handleException(e: Exception): ResponseEntity<ApiResponse<Any>> {
+        MDC.put("status", "500")
+        logging(e, true)
+        return ResponseEntity(ApiResponse.error(ErrorType.DEFAULT_ERROR, e.message), ErrorType.DEFAULT_ERROR.status)
+    }
 
     @ExceptionHandler(CoreException::class)
-    fun handleCoreException(e: CoreException): ResponseEntity<ApiResponse<Any>> = ResponseEntity(ApiResponse.error(e.coreErrorType, e.data), ErrorType.COMMON_ERROR.status)
+    fun handleCoreException(e: CoreException): ResponseEntity<ApiResponse<Any>> {
+        MDC.put("status", "400")
+        logging(e)
+        return ResponseEntity(ApiResponse.error(e.coreErrorType, e.data), ErrorType.COMMON_ERROR.status)
+    }
+
+    private fun logging(e: Throwable, includeFullStackTrace: Boolean = false) {
+        if (includeFullStackTrace) {
+            MDC.put("exceptionFull", e.stackTraceToString())
+        }
+        logger.error("Exception occurred", e)
+        MDC.clear()
+    }
+
+    companion object {
+        private val logger = LoggerFactory.getLogger("ErrorLog")
+    }
 }

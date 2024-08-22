24becb8ae3a8378b748a8b59a7e1098165357aa2
Jang Hojin <96263955+hojinida@users.noreply.github.com>
2024-08-22 20:16:55 +0900

    [FEAT] logging (#107)
    * [FEAT] Host, X-ForwarddedFor , X-Forwarded-Proto 요청로그 추가, 응답 로거 구현 (#103)
    
    * [FEAT] xml 설정에서 추가한 값 주입, IP,Host,XForwarded 값 주입 위해 nginx 설정 수정 (#103)
    
    * [FEAT] 로그 파일을 덮어쓰도록 수정 (#103)
    
    * [FIX] Put -> Patch (#103)

--- core/core-api/src/main/kotlin/com/myongjiway/core/api/controller/NoticeAdminController.kt
+++ core/core-api/src/main/kotlin/com/myongjiway/core/api/controller/NoticeAdminController.kt
@@ -6,9 +6,9 @@ import com.myongjiway.core.domain.notice.NoticeService
 import com.myongjiway.core.domain.user.User
 import org.springframework.security.core.annotation.AuthenticationPrincipal
 import org.springframework.web.bind.annotation.DeleteMapping
+import org.springframework.web.bind.annotation.PatchMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -24,7 +24,7 @@ class NoticeAdminController(
         return ApiResponse.success()
     }
 
-    @PutMapping("/{noticeId}")
+    @PatchMapping("/{noticeId}")
     fun updateNotice(@PathVariable noticeId: Long, @RequestBody noticeRequest: NoticeRequest, @AuthenticationPrincipal user: User): ApiResponse<Any> {
         noticeService.updateNotice(noticeRequest.toMetaData(user.name), noticeId)
         return ApiResponse.success()

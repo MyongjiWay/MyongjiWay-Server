ca47d4811490504c856a30d731d7a22c10c46438
hojinida <opqrs1103@gmail.com>
2024-08-22 20:43:14 +0900

    [FEAT] Async Enable (#108)

--- core/core-api/src/main/kotlin/com/myongjiway/CoreApiApplication.kt
+++ core/core-api/src/main/kotlin/com/myongjiway/CoreApiApplication.kt
@@ -3,7 +3,9 @@ package com.myongjiway
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableAsync
 
+@EnableAsync
 @ConfigurationPropertiesScan
 @SpringBootApplication
 class CoreApiApplication
